bdc7f444fe9cc89f234f266090ba067722adc14f
“testerjob” <zaytsevalexey77@gmail.com>
2019-10-03 13:11:53 -0700

    task8.py

--- task8.py
+++ task8.py
@@ -1,6 +1,7 @@
 #Make a two-player Rock-Paper-Scissors game. (Hint: Ask for player plays (using input), compare them, 
 #print out a message of congratulations to the winner, and ask if the players want to start a new game)
 
+
 import sys
 
 print("Please choose one: \n-Rock\n-Paper\n-Scissors ")
@@ -9,28 +10,48 @@ user1 = user1.lower()
 user2 = input("Please enter your choice: ")
 user2 = user2.lower()
 
-while user1 == user2:
-	print("The choise is even, please start all over again: ")
-	print("Please choose one: \n-Rock\n-Paper\n-Scissors ")
-	user1 = input("Please enter your choise: ")
-	user1 = user1.lower()
-	user2 = input("Please enter your choice: ")
-	user2 = user2.lower()
+#I want to check if user imput is correct and loop it
+temp = set([ 'rock', 'scissors', 'paper'] )
+
+while True:
+    if user1 not in temp and user2 not in temp:
+        print('Please check your spell and type correct word: ')
+    
+    elif user1 not in temp and user2 in temp:
+        print('Please check your spell and type correct word: ')
+
+    elif user2 not in temp and user1 in temp:
+        print('Please check your spell and type correct word: ')
+
+    elif user1 == user2:
+        print("The choise is even, please start all over again: ")
+        print("Please choose one: \n-Rock\n-Paper\n-Scissors ")
+    else:
+        break
+
+    user1 = input("Please enter your choise: ")
+    user1 = user1.lower()
+    user2 = input("Please enter your choice: ")
+    user2 = user2.lower()
 
 
-if user1 == 'rock' or user2 == 'scissors':
-	print("User 1 is a WINNER! ")
-elif user1 == 'paper' or user2 == 'scissors':
-	print("User 2 is the WINNER! ")
-elif user1 == 'paper' or user2 == 'rock':
-	print("User 1 is a WINNER! ")
-elif user1 == 'scissors' or user2 == 'rock':
-	print("User 2 is a WINNER! ")
-elif user1 == 'scissors' or user2 == 'paper':
-	print("User 1 is a WINNER! ")
-else:
-	print("Please check your spell and start it again: ")
+if user1 == 'scissors' and user2 == 'rock':
+    print("User 2 is a WINNER! ")
 
+elif user2 == 'scissors' and user1 == 'rock':
+    print("User 1 is a WINNER! ")
+
+elif user1 == 'paper' and user2 == 'rock':
+    print("User 1 is the WINNER! ")
+
+elif user2 == 'paper' and user1 == 'rock':
+    print("User 2 is the WINNER! ")
+
+elif user1 == 'scissors' and user2 == 'paper':
+    print("User 1 is the WINNER! ")
+
+elif user2 == 'scissors' and user1 == 'paper':
+    print("User 2 is the WINNER! ")
 
 
 
